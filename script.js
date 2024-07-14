48fef3570452dcea72425a7c21cda29535df46e7
Bevilhonda <marcelinyuri@gmail.com>
2024-07-14 16:22:16 -0300

    atualiza função criptografar

--- script.js
+++ script.js
@@ -13,46 +13,26 @@
  caracteres que não são letras minúsculas (a-z) ou espaços (\s) por uma string vazia ('').
   Em outras palavras, remove todos os caracteres que não são letras minúsculas ou espaços.*/
 
-  document.getElementById('inputText').addEventListener('keydown',function(event){
 
-    // o evento keydown serve para capturar quando o usuário pressiona uma tecla.
+function mensagemDeErro(message) {
+  let caixaDeMensagem = document.createElement('div');
+  caixaDeMensagem.className = 'message-box';
+  caixaDeMensagem.textContent = message;
+  document.body.appendChild(caixaDeMensagem);
 
-    if (event.key === 'Enter') { // e aqui verifica se a tecla pressionada foi "Enter".
-    let campoTexto = event.target;
-    let textoDigitado = campoTexto.value;
-
-    if (/[A-ZÀ-Úà-ú]/.test(textoDigitado)) {
-      mensagemDeErro("Apenas letras minúsculas e sem acentos são permitidas.");
-
-      campoTexto.value = '';
-    }
-  }
-    /* se o usuário digitar uma palavra ou texto que contenha letra maiúscula ou 
-    com acentos, vai gerar o alert e resetar o campo onde digita o texto.
-    */
-
-  });
-
-  function mensagemDeErro(message) {
-    let caixaDeMensagem = document.createElement('div');
-    caixaDeMensagem.className = 'message-box';
-    caixaDeMensagem.textContent = message;
-    document.body.appendChild(caixaDeMensagem);
-  
-    // Remover a mensagem após alguns segundos (opcional)
-    setTimeout(function() {
-      document.body.removeChild(caixaDeMensagem);
-    }, 5000); 
-    // esse numero de 5000 equivale á 5 segundos , é o tempo que a mensagem fica na tela
-  }
-
-
-let outputText = document.getElementById("outputText");
-let afterResult = document.querySelector(".campo_resultado_visual");
-let buttonCopy = document.querySelector(".botao_copiar");
-let boxResult = document.querySelector(".campo_resultado");
+  // essa função remove a mensagem após alguns segundos. 
+  setTimeout(function () {
+    document.body.removeChild(caixaDeMensagem);
+  }, 5000);
+  // esse numero de 5000 equivale á 5 segundos , é o tempo que a mensagem fica na tela
+}
 
 function adjustStyle() {
+  let afterResult = document.querySelector(".campo_resultado_visual");
+  let buttonCopy = document.querySelector(".botao_copiar");
+  let boxResult = document.querySelector(".campo_resultado");
+  let outputText = document.getElementById("outputText");
+
   afterResult.style.display = "none";
   buttonCopy.style.display = "block";
   boxResult.style.justifyContent = "space-between";
@@ -61,26 +41,43 @@ function adjustStyle() {
 }
 
 function criptografar() {
-  let text = document.getElementById("inputText").value;
-  let encryptedText = text
-    .replace(/e/g, "enter")
-    .replace(/i/g, "imes")
-    .replace(/a/g, "ai")
-    .replace(/o/g, "ober")
-    .replace(/u/g, "ufat");
-  outputText.innerText = encryptedText;
-  adjustStyle();
+  let campoTexto = document.getElementById("inputText");
+  let textoDigitado = campoTexto.value;
+
+  if (/[A-ZÀ-Úà-ú]/.test(textoDigitado)) {
+    mensagemDeErro("Apenas letras minúsculas e sem acentos são permitidas.");
+    campoTexto.value = '';
+    return;
+  }
+  /* se o usuário digitar uma palavra ou texto que contenha letra maiúscula ou 
+    com acentos, vai gerar o alert e resetar o campo onde digita o texto.
+    */
+  else {
+    let textoCriptografado = textoDigitado
+      .replace(/e/g, "enter")
+      .replace(/i/g, "imes")
+      .replace(/a/g, "ai")
+      .replace(/o/g, "ober")
+      .replace(/u/g, "ufat");
+
+    let outputText = document.getElementById("outputText");
+    outputText.innerText = textoCriptografado;
+
+    adjustStyle();
+  }
 }
 
 function descriptografar() {
   let text = document.getElementById("inputText").value;
-  let encryptedText = text
+  let textoDescriptografado = text
     .replace(/enter/g, "e")
     .replace(/imes/g, "i")
     .replace(/ai/g, "a")
     .replace(/ober/g, "o")
     .replace(/ufat/g, "u");
-  outputText.innerText = encryptedText;
+  let outputText = document.getElementById("outputText");
+  outputText.innerText = textoDescriptografado;
+
   adjustStyle();
 }
 
@@ -90,3 +87,5 @@ function copiarParaAreaInput() {
   text.setSelectionRange(0, 99999);
   navigator.clipboard.writeText(text.value);
 }
+
+document.querySelector('.button-discr').addEventListener('click', descriptografar);
